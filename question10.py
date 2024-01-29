e6a12811390afe2d38e844ea4acc4bc7408138bf
Teja2121 <56876637+Teja2121@users.noreply.github.com>
2024-01-29 10:01:00 -0500

    question10.py

--- question10.py
+++ question10.py
@@ -2,51 +2,51 @@ def question10_1():
     """
     answer: python bool (True or False)
     """
-    answer: python bool (False)
+    answer: bool = False
     # Descripton : Count data can sometimes be affected by noise like measurement errors.
     return answer
 
 def question10_2():
-    answer: python bool (True)
+    answer: bool = True
     # Justification : The value of correlation coefficient ranges from -1 (Perfect negative correlation) to 1 (Perfect positive correlation).
     return answer
 
 def question10_3():
-    answer: python bool (True)
+    answer: bool = True
     # Justification : Sampling is a simpler process becuase aggregation may involve complex calculations.
     return answer
 
 def question10_4():
-    answer: python bool (True)
+    answer: bool = True
     # Justification : Sometimes noise and outliers can be the same, especially when an outlier behaves like noise and is changing the results significantly.
     return answer
 
 def question10_5():
-    answer: python bool (False)
+    answer: bool = False
     # Justification : Not every case of outlier can be a noise, because some outliers might be valid.
     return answer
 
 def question10_6():
-    answer: python bool (False)
+    answer: bool = False
     # Justification : Not every binary attribute can be an assymetric binary attribute because sometimes 0 or 1 can be given equal importance. 
     return answer
 
 def question10_7():
-    answer: python bool (False)
+    answer: python = False
     # Justification : The objects might not be identical, because their magnitudes might be different and cosine similarity doesn't account for that.
     return answer
 
 def question10_8():
-    answer: python bool (False)
+    answer: bool = False
     # Justification : Discrete variables can be sometimes ratio. Like the number of cars in a parking lot (it can never be infinite)
     return answer
 
 def question10_9():
-    answer: python bool (False)
+    answer: bool = False
     # Justification : Quantitative variables can be discrete too. 
     return answer
 
 def question10_10():
-    answer: python bool (False)
+    answer: bool = False
     # Justification : There might be some loss like loss in the ordering of the variables.
-    return answer
+    return answer
