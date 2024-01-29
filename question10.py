357f4f5565b83c28e3f69cae0fa7400f18922c0c
Teja2121 <56876637+Teja2121@users.noreply.github.com>
2024-01-29 09:53:11 -0500

    question10.py

--- question10.py
+++ question10.py
@@ -3,50 +3,50 @@ def question10_1():
     answer: python bool (True or False)
     """
     answer: python bool (False)
-    justify = 'Count data can sometimes be affected by noise like measurement errors.'
+    # Descripton : Count data can sometimes be affected by noise like measurement errors.
     return answer
 
 def question10_2():
     answer: python bool (True)
-    justify =  'The value of correlation coefficient ranges from -1 (Perfect negative correlation) to 1 (Perfect positive correlation).'
+    # Description : The value of correlation coefficient ranges from -1 (Perfect negative correlation) to 1 (Perfect positive correlation).
     return answer
 
 def question10_3():
     answer: python bool (True)
-    justify =  'Sampling is a simpler process because aggregation may involve complex calculations.'
+    # Description : Sampling is a simpler process becuase aggregation may involve complex calculations.
     return answer
 
 def question10_4():
     answer: python bool (True)
-    justify =  'Sometimes noise and outliers can be the same, especially when an outlier behaves like noise and is changing the results significantly.'
+    # Description : Sometimes noise and outliers can be the same, especially when an outlier behaves like noise and is changing the results significantly.
     return answer
 
 def question10_5():
     answer: python bool (False)
-    justify =  'Not every case of outlier can be a noise, because some outliers might be valid.'
+    # Description : Not every case of outlier can be a noise, because some outliers might be valid.
     return answer
 
 def question10_6():
     answer: python bool (False)
-    justify =  'Not every binary attribute can be an assymetric binary attribute because sometimes 0 or 1 can be given equal importance.'
+    # Description : Not every binary attribute can be an assymetric binary attribute because sometimes 0 or 1 can be given equal importance. 
     return answer
 
 def question10_7():
     answer: python bool (False)
-    justify =  'The objects might not be identical, because their magnitudes might be different and cosine similarity doesn't account for that.'
+    # Description : The objects might not be identical, because their magnitudes might be different and cosine similarity doesn't account for that.
     return answer
 
 def question10_8():
     answer: python bool (False)
-    justify =  'Discrete variables can be sometimes ratio. Like the number of cars in a parking lot (it can never be infinite).'
+    # Description : Discrete variables can be sometimes ratio. Like the number of cars in a parking lot (it can never be infinite)
     return answer
 
 def question10_9():
     answer: python bool (False)
-    justify =  'Quantitative variables can be discrete too.'
+    # Description : Quantitative variables can be discrete too. 
     return answer
 
 def question10_10():
     answer: python bool (False)
-    justify =  'There might be some loss like loss in the ordering of the variables.'
-    return answer
+    # Description : There might be some loss like loss in the ordering of the variables.
+    return answer
