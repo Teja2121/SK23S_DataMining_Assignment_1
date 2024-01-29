50a7f85bc6a11deb7a2d6ac6be5774af2c08c7c1
Teja2121 <56876637+Teja2121@users.noreply.github.com>
2024-01-29 02:25:01 -0500

    question3.py

--- question3.py
+++ question3.py
@@ -1,24 +1,34 @@
 def question3_1():
     # answer = ['string', 'string', 'string']
+    answer = ['continuous', 'quantitative', 'interval']
     return answer
 
 def question3_2():
+    answer = ['discrete', 'qualitative', 'ordinal']
     return answer
 
 def question3_3():
+    answer = ['discrete', 'qualitative', 'ordinal']
     return answer 
 
 def question3_4():
+    answer = ['continuous', 'quantitative', 'ratio']
     return answer
 
 def question3_5():
+    answer = ['discrete', 'qualitative', 'nominal']
     return answer
 
 def question3_6():
+    answer = ['discrete', 'quantitative', 'ratio']
+    # assumption in asnwering : assuming a richter scale of 0-10
     return answer
 
 def question3_7():
+    answer = ['continuous', 'quantitative', 'ratio']
+    # assumption in answering : Median can be true zero too
     return answer
 
 def question3_8():
+    answer = ['discrete', 'qualitative', 'ordinal']
     return answer
