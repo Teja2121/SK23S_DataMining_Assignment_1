25c978598645f1134f7ebf9d53352c1334f8589f
Teja2121 <56876637+Teja2121@users.noreply.github.com>
2024-01-29 09:55:43 -0500

    question10.py

--- question10.py
+++ question10.py
@@ -8,45 +8,45 @@ def question10_1():
 
 def question10_2():
     answer: python bool (True)
-    # Description : The value of correlation coefficient ranges from -1 (Perfect negative correlation) to 1 (Perfect positive correlation).
+    # Justification : The value of correlation coefficient ranges from -1 (Perfect negative correlation) to 1 (Perfect positive correlation).
     return answer
 
 def question10_3():
     answer: python bool (True)
-    # Description : Sampling is a simpler process becuase aggregation may involve complex calculations.
+    # Justification : Sampling is a simpler process becuase aggregation may involve complex calculations.
     return answer
 
 def question10_4():
     answer: python bool (True)
-    # Description : Sometimes noise and outliers can be the same, especially when an outlier behaves like noise and is changing the results significantly.
+    # Justification : Sometimes noise and outliers can be the same, especially when an outlier behaves like noise and is changing the results significantly.
     return answer
 
 def question10_5():
     answer: python bool (False)
-    # Description : Not every case of outlier can be a noise, because some outliers might be valid.
+    # Justification : Not every case of outlier can be a noise, because some outliers might be valid.
     return answer
 
 def question10_6():
     answer: python bool (False)
-    # Description : Not every binary attribute can be an assymetric binary attribute because sometimes 0 or 1 can be given equal importance. 
+    # Justification : Not every binary attribute can be an assymetric binary attribute because sometimes 0 or 1 can be given equal importance. 
     return answer
 
 def question10_7():
     answer: python bool (False)
-    # Description : The objects might not be identical, because their magnitudes might be different and cosine similarity doesn't account for that.
+    # Justification : The objects might not be identical, because their magnitudes might be different and cosine similarity doesn't account for that.
     return answer
 
 def question10_8():
     answer: python bool (False)
-    # Description : Discrete variables can be sometimes ratio. Like the number of cars in a parking lot (it can never be infinite)
+    # Justification : Discrete variables can be sometimes ratio. Like the number of cars in a parking lot (it can never be infinite)
     return answer
 
 def question10_9():
     answer: python bool (False)
-    # Description : Quantitative variables can be discrete too. 
+    # Justification : Quantitative variables can be discrete too. 
     return answer
 
 def question10_10():
     answer: python bool (False)
-    # Description : There might be some loss like loss in the ordering of the variables.
+    # Justification : There might be some loss like loss in the ordering of the variables.
     return answer
