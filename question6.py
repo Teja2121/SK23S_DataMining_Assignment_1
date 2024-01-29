57083014fc258cb9d96930286c17d1656f0ebcf0
Teja2121 <56876637+Teja2121@users.noreply.github.com>
2024-01-29 18:34:29 -0500

    question6.py

--- question6.py
+++ question6.py
@@ -21,7 +21,7 @@ def question6_2():
 def question6_3():
     answer = {
         'equal_width': ['change', 2],
-        'equal_frequency': ['no change', 2]
+        'equal_frequency': ['no change', 9]
     }
     return answer 
 
