ff9038c6c5c25815d643cade645bc4579ece5321
Teja2121 <56876637+Teja2121@users.noreply.github.com>
2024-01-29 20:33:18 -0500

    question7.py

--- question7.py
+++ question7.py
@@ -20,5 +20,6 @@ def question7_3():
     """
     answer = [(0.,2.), 
               (2.,3.), 
-              (3.,5.), (5.,'infinity')]
+              (3.,5.), 
+              (5.,'infinity')]
     return answer 
